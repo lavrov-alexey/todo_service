546f45aadf4c1298f7a6b067d81cd023b5ad252a
lavrov-alexey <lavrov-box@mail.ru>
2023-03-01 21:32:11 +0300

    Lesson-02. Add menu and footer

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,33 @@
+// import React from "react";
+//
+// const MenuContainer = () => {
+//     return(
+//         <div>
+//             <table>
+//                 <th>Users List</th>
+//                 <th>TODOs List</th>
+//                 <th>Projects List</th>
+//             </table>
+//         </div>)
+// }
+//
+// export default MenuContainer
+
+import React from "react";
+import {Container, Navbar} from "react-bootstrap"
+
+const Menu = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <th><Navbar.Brand href="#">Users list</Navbar.Brand></th>
+                    <th><Navbar.Brand href="#">Projects list</Navbar.Brand></th>
+                    <th><Navbar.Brand href="#">TODOs list</Navbar.Brand></th>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Menu
