a9adfe3dfcfc74c67c4e354fe62c1d381b466f06
lavrov-alexey <lavrov-box@mail.ru>
2023-04-05 22:13:43 +0300

    Lesson-08. Add APIRequestFactory test

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from .views import ProjectModelViewSet
 
-# Create your tests here.
+# Базовый тест (нижний уровень) на основе тестов Джанго
+class ProjectTestCase(TestCase):
+
+    # все методы, начинающиеся на test... будут интерпретироваться как тесты и соотв. образом отображаться
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        # ловим ошибку доступа по неавторизованному запросу (не залогинены)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
