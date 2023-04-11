aec9d8481cd36c9c6b8158a35423ee9b1d56dda7
lavrov-alexey <lavrov-box@mail.ru>
2023-04-11 07:16:38 +0300

    Lesson-10. Get by id

--- todo/graphql.py
+++ todo/graphql.py
@@ -19,10 +19,21 @@ class TodoObjectType(DjangoObjectType):
         fields = '__all__'
 
 
+class UserObjectType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 class Query(graphene.ObjectType):
-    # описываем в схеме команду и какой тип данных будет возвращен (список определенных нами объектов проектов)
+    # описываем в схеме команду и в 1ом параметре - передать какой тип данных будет возвращен
+    # (напр. список определенных нами объектов проектов)
     all_projects = graphene.List(ProjectObjectType)
     all_todo = graphene.List(TodoObjectType)
+    all_users = graphene.List(UserObjectType)
+    # для получ. по id - нужно передать тип возвращаемых данных и входные - этот id и указываем, что это обяз. параметр
+    get_project_by_id = graphene.Field(ProjectObjectType, pk=graphene.Int(required=True))
+    get_user_by_id = graphene.Field(UserObjectType, pk=graphene.Int(required=True))
 
     # описываем собственно саму команду - начинается с "resolve_" и имени команды
     def resolve_all_projects(self, info):
@@ -30,6 +41,15 @@ class Query(graphene.ObjectType):
 
     def resolve_all_todo(self, info):
         return Todo.objects.all()
+
+    def resolve_all_users(self, info):
+        return User.objects.all()
+
+    def resolve_get_project_by_id(self, info, pk):
+        return Project.objects.get(pk=pk)
+
+    def resolve_get_user_by_id(self, info, pk):
+        return User.objects.get(pk=pk)
 
 # class Query(graphene.ObjectType):  # первичная тренировка
 #     # описываем в схеме команду и какой тип данных будет возвращен
