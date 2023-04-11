b92dac50b65b7d34af75b6f352c86353223f9667
lavrov-alexey <lavrov-box@mail.ru>
2023-04-11 06:16:05 +0300

    Lesson-10. Complex object view

--- todo/graphql.py
+++ todo/graphql.py
@@ -12,13 +12,24 @@ class ProjectObjectType(DjangoObjectType):
         model = Project  # указываем модель из Джанги
         fields = '__all__'  # и список нужных полей из модели
 
+
+class TodoObjectType(DjangoObjectType):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
 class Query(graphene.ObjectType):
     # описываем в схеме команду и какой тип данных будет возвращен (список определенных нами объектов проектов)
     all_projects = graphene.List(ProjectObjectType)
+    all_todo = graphene.List(TodoObjectType)
 
     # описываем собственно саму команду - начинается с "resolve_" и имени команды
     def resolve_all_projects(self, info):
         return Project.objects.all()
+
+    def resolve_all_todo(self, info):
+        return Todo.objects.all()
 
 # class Query(graphene.ObjectType):  # первичная тренировка
 #     # описываем в схеме команду и какой тип данных будет возвращен
